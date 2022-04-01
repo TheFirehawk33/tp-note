18c85104e64b664c99566f159782c55eea668514
Alex Amemoutou <mr.say6@gmail.com>
2022-04-01 10:00:38 +0200

    Add Offer creation and test (#1)
    * add test create
    
    * Add builder and OfferTypeEnum
    
    * Patch test
    
    Co-authored-by: AlexAmemoutou <alex.amemoutou@gmail.com>

--- src/main/java/com/hemit/models/Candidate.java
+++ src/main/java/com/hemit/models/Candidate.java
@@ -1,25 +1,37 @@
 package com.hemit.models;
 
+import com.hemit.utils.OfferTypeEnum;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+
 public class Candidate extends PanacheMongoEntity {
+    @NotEmpty
     public String firstName;
+    @NotEmpty
     public String lastName;
     public String birthDate;
+    @Email
     public String email;
     public String profilePictureURL;
+    @NotEmpty
     public String description;
-    public float minimumSalary;
+    @NotNull
+    public Float minimumSalary;
     public List<String> offerIds;
-    public List<String> offerTypes;
+    @NotEmpty
+    public List<OfferTypeEnum> offerTypes;
+    @NotNull
     public List<String> keywords;
     public String lastLoggedIn;
 
     public Candidate() {}
 
-    public Candidate(String firstName, String lastName, String birthDate, String email, String profilePictureURL, String description, float minimumSalary, List<String> offerIds, List<String> offerTypes, List<String> keywords, String lastLoggedIn) {
+    public Candidate(String firstName, String lastName, String birthDate, String email, String profilePictureURL, String description, float minimumSalary, List<String> offerIds, List<OfferTypeEnum> offerTypes, List<String> keywords, String lastLoggedIn) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
@@ -31,5 +43,70 @@ public class Candidate extends PanacheMongoEntity {
         this.offerTypes = offerTypes;
         this.keywords = keywords;
         this.lastLoggedIn = lastLoggedIn;
+    }
+
+    public Candidate setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public Candidate setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+
+    }
+
+    public Candidate setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+        return this;
+
+    }
+
+    public Candidate setEmail(String email) {
+        this.email = email;
+        return this;
+
+    }
+
+    public Candidate setProfilePictureURL(String profilePictureURL) {
+        this.profilePictureURL = profilePictureURL;
+        return this;
+
+    }
+
+    public Candidate setDescription(String description) {
+        this.description = description;
+        return this;
+
+    }
+
+    public Candidate setMinimumSalary(Float minimumSalary) {
+        this.minimumSalary = minimumSalary;
+        return this;
+
+    }
+
+    public Candidate setOfferIds(List<String> offerIds) {
+        this.offerIds = offerIds;
+        return this;
+
+    }
+
+    public Candidate setOfferTypes(List<OfferTypeEnum> offerTypes) {
+        this.offerTypes = offerTypes;
+        return this;
+
+    }
+
+    public Candidate setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+        return this;
+
+    }
+
+    public Candidate setLastLoggedIn(String lastLoggedIn) {
+        this.lastLoggedIn = lastLoggedIn;
+        return this;
+
     }
 }
